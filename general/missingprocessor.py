097d5ba1d9ae179da5f8cb0fcdcd937c4034a3ca
acphile <phile_999@126.com>
2021-11-16 06:08:33 +0800

    add lisense

--- general/missingprocessor.py
+++ general/missingprocessor.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 from sklearn.preprocessing import MinMaxScaler, RobustScaler, LabelBinarizer
 import numpy as np
 import pandas as pd
